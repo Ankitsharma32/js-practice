9d3f8b0e0b7d547e128ca9c5ba164f5cac84f406
Ankit Sharma <123965682+Ankitsharma32@users.noreply.github.com>
2024-03-13 17:45:16 +0000

    config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all Js as a newer version
+
+// alert(3+3) //if we are using node js then we get error on it
+
+console.log(3+3); 
+console.log("Ankit")
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 1548
+let accountEmail = "ankitsharma@gmail.com"
+var accountPassword = "12345"
+accountcity = "mumbai"
+
+// accountId = 2 //not allowed
+
+accountEmail = "hello@hc.com"
+accountPassword = "12548"
+accountcity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountcity])
